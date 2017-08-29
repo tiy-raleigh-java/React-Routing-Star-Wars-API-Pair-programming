38b11d7f07a3ec23f5db11b0c717309442efeaba
newline-power-tools <doug@theironyard.com>
2017-08-29 17:36:48 +0000

    6e9bdb3b-starter_files.zip

---
+++ starter_files/src/index.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/index.css';
+import './styles/App.css';
+import registerServiceWorker from './registerServiceWorker';
+
+// IMPORT REACT ROUTER <<<<<<<<<<<<<<<<<<<<<<
+
+// IMPORT COMPONENTS <<<<<<<<<<<<<<<<<<<<<<
+
+
+ReactDOM.render(
+
+        {/* >>>>>>>>>>>>>>>>>>>>>
+          Add a router components with routes to:
+          Explorer. Use an exact path to "/"
+          Starships
+          Films
+          People
+          <<<<<<<<<<<<<<<<<<<<<<
+          */}
+
+
+,document.getElementById('root'));
+registerServiceWorker();
